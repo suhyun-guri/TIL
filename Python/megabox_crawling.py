9f411e7fe7e912e14d7e195d0ca9e0cd74db855e
Suhyun <suhyun.kim@imweb.me>
2023-07-25 21:17:05 +0900

    [Python] Megabox 예매 오픈 크롤링 및 슬랙 메시지 보내기 Update

--- Python/megabox_crawling.py
+++ Python/megabox_crawling.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 import json, sys
 from datetime import datetime
 
-def send_message(when, where, moviename):
+def send_message(when, where, moviename, starttime):
   
-  # 생성한 웹훅 주소
-  hook = '' # 웹훅 주소 
-  content = f":party-parrot-mal-mal: {when} {where} {moviename} 메박 떴다"
+  # 생성한 Slack 웹훅 주소
+  hook = ''
+  content = f":party-parrot-mal-mal: {when} {where} {moviename} {starttime} 메박 떴다"
   
   # 메시지 전송
   requests.post(
@@ -15,7 +15,7 @@ def send_message(when, where, moviename):
     json={
       'blocks': [
         {
-          'type': 'section',  # 저는 메시지가 묶이지 않도록 section을 주로 사용합니다.
+          'type': 'section', 
           'text': {
             'type': 'mrkdwn',
             'text': content
@@ -30,22 +30,19 @@ url = 'https://www.megabox.co.kr/on/oh/ohc/Brch/schedulePage.do'
 
 params = {"brchNo": "1351", 
           "brchNo1": "1351",
-          "crtDe": "20230527",
+          "crtDe": "20230726",
           "detailType": "area",
           "firstAt": "N",
           "masterType": "brch",
-          "playDe": "20230531"}
+          "playDe": "20230726"}
 
 response = requests.post(url, params=params).json()
 
 
 for i in response['megaMap']['movieFormList']:
-    if i['movieNo'] == '23018401':
-        # print(i)
-        send_message(i['playDe'], i['brchNm'], i['movieNm'])
-        break
-    else:
-        print(f"{datetime.now()} 없음")
+    if (i['movieNo'] == '23048900') & (i['playStartTime'].startswith('19')):
+        print(i)
+        send_message(i['playDe'], i['brchNm'], i['movieNm'], i['playStartTime'])
         break
         
         
